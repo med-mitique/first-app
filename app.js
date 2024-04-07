e6216b76112bc22890d48edf50a1b7611377a4e3
Med Mitique <161362959+med-mitique@users.noreply.github.com>
2024-04-07 10:44:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, rep)=>{
-    rep.sendFile(__dirname + "/views/index.html");
+    rep.send("Home Page");
 });
 
 app.get('/homework', (req, rep) => {
     rep.send('<h1>Homework Page</h1>');
-
 });
 
 app.listen(80, ()=>console.log("Listing on port 80."));
